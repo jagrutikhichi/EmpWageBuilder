28958f5e6640f723cca20653a168ba73cf91d9a3
jagrutikhichi <jagrutikhichi123@gmail.com>
2021-06-22 11:51:40 +0530

    Modified: to calculate Emp wage with condition of max Day or max Hrs

--- src/com/bridgelabes/EmpWageBuilder.java
+++ src/com/bridgelabes/EmpWageBuilder.java
@@ -5,32 +5,43 @@ public class EmpWageBuilder {
 	public static final int IS_EMP_PRESENT = 1;
 	public static final int IS_PART_TIME= 2;
 	public static final int WAGE_PER_HOUR = 20;
-	public static final int FULLDAY = 8;
-	public static final int PART_TIME=4;
 	public static final int NUM_OF_WROKING_DAY=20;
+	public static final int MAX_HRS_ALLOWED = 100;
+
 
 	public static void main(String[] args) 
 	{
 		int monthlyEmpWage = 0;
-		int dailywage = 0;
-		for (int day = 0; day < NUM_OF_WROKING_DAY; day++) 
+		int totalWorkingDays = 0;
+		int totalEmpHrs = 0;
+		
+		while (totalEmpHrs < MAX_HRS_ALLOWED && totalWorkingDays < NUM_OF_WROKING_DAY)
 		{
+			totalWorkingDays++ ;
+			int dailywage = 0;
+			int empHrs = 0;
+			
 			double empCheck = Math.floor(Math.random() * 10) % 3;
+			
 
 			switch ((int) empCheck) {
 			case IS_EMP_PRESENT:
-				dailywage = FULLDAY * WAGE_PER_HOUR;
+				empHrs= 8;
 				break;
 			case IS_PART_TIME:
-				dailywage = PART_TIME * WAGE_PER_HOUR;				
+				empHrs = 4;
 				break;
 			default:
 				System.out.println("Emp is Absent");
 			}
+			totalEmpHrs = totalEmpHrs + empHrs;
+			dailywage = WAGE_PER_HOUR * empHrs ;
 			monthlyEmpWage = monthlyEmpWage + dailywage ;
+			
 			System.out.println("Emp Wage is : "+dailywage);
 			
 		}
 		System.out.println("monthly wage : " +monthlyEmpWage);
+		System.out.println("total Hrs : " +totalEmpHrs);
 	}
 }
