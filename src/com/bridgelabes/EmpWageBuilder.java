e01666802de4320a17cb03074f31517779a08768
jagrutikhichi <jagrutikhichi123@gmail.com>
2021-06-22 09:31:49 +0530

    Updated: Employe wage builder for daily wage

--- src/com/bridgelabes/EmpWageBuilder.java
+++ src/com/bridgelabes/EmpWageBuilder.java
@@ -2,17 +2,25 @@ package com.bridgelabes;
 
 public class EmpWageBuilder {
 
-	public static final int IS_EMP_PRESENT= 1;
-	
-	 
-		public static void main(String[] args) {
-					
-			double empCheck = Math.floor(Math.random()*10)%2;
-			if(empCheck == 1) {
-				System.out.println("Emp is present");
-			}
-			else {
-				System.out.println("Emp is absent");
-			}
+	public static final int IS_EMP_PRESENT = 1;
+	public static final int WAGE_PER_HOUR = 20;
+	public static final int FULLDAY = 8;
+
+	public static void main(String[] args) 
+	{
+		int dailywage = 0;
+		double empCheck = Math.floor(Math.random() * 10) % 2;
+		if (empCheck == 1) 
+		{
+			System.out.println("Emp is present");
+			dailywage = FULLDAY * WAGE_PER_HOUR;
+			System.out.println("Daily Emp Wage is : " + dailywage);
+		} 
+		else 
+		{
+			System.out.println("Emp is absent");
+
 		}
+		System.out.println("Daily Emp Wage is : " + dailywage);
+	}
 }
